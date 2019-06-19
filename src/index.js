458fb222e37bf54b10d7f8c07e690cc99c321301
Rory Murray <rory.christian.murray@gmail.com>
2019-06-19 16:09:31 -0400

    Initial commit and basic scaffolding

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 // needed dependancies
-// applyMiddleware from redux
-// thunk from redux-thunk
-// logger from redux-logger
-// rootReducer from ./reducers
+// applyMiddleware from redux [DONE]
+// thunk from redux-thunk [DONE]
+// logger from redux-logger [DONE]
+// rootReducer from ./reducers [DONE]
 
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
